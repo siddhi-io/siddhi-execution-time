25ac463fd1ac169432a57797128c0389ec8ae89f
dnwick <dnsnipe123@gmail.com>
2020-11-18 19:48:25 +0530

    Add improvement to allow timezone conversion of a given date

---
+++ component/src/main/java/io/siddhi/extension/execution/time/TimezoneConvertFunctionExtension.java
@@ -0,0 +1,232 @@
+/*
+ *
+ *  * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *  *
+ *  * WSO2 Inc. licenses this file to you under the Apache License,
+ *  * Version 2.0 (the "License"); you may not use this file except
+ *  * in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied. See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package io.siddhi.extension.execution.time;
+
+/**
+ * timezoneConvert(dateValue,dateSourceFormat,targetTimezone,sourceTimezone)
+ * Returns the converted date string of same format as source.
+ * dateValue - value of date. eg: "2014-11-11 13:23:44.657", "2014-11-11" , "13:23:44.657"
+ * dateSourceFormat - Date format of the provided date value. eg: yyyy-MM-dd HH:mm:ss.SSS
+ * targetTimezone - Time zone which the date value need to be converted to
+ * sourceTimezone - Time zone which source date belongs to
+
+ * Accept Type(s) for timezoneConvert(dateValue,dateSourceFormat,sourceTimezone,targetTimezone)
+ * dateValue : STRING
+ * dateSourceFormat : STRING
+ * sourceTimezone : STRING
+ * targetTimezone : STRING
+ *
+ */
+
+import io.siddhi.annotation.Example;
+import io.siddhi.annotation.Extension;
+import io.siddhi.annotation.Parameter;
+import io.siddhi.annotation.ParameterOverload;
+import io.siddhi.annotation.ReturnAttribute;
+import io.siddhi.annotation.util.DataType;
+import io.siddhi.core.config.SiddhiQueryContext;
+import io.siddhi.core.exception.SiddhiAppRuntimeException;
+import io.siddhi.core.executor.ExpressionExecutor;
+import io.siddhi.core.executor.function.FunctionExecutor;
+import io.siddhi.core.util.config.ConfigReader;
+import io.siddhi.core.util.snapshot.state.State;
+import io.siddhi.core.util.snapshot.state.StateFactory;
+import io.siddhi.query.api.definition.Attribute;
+import io.siddhi.query.api.exception.SiddhiAppValidationException;
+import org.apache.log4j.Logger;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Class representing the Timezone conversion implementation.
+ */
+@Extension(
+        name = "timezoneConvert",
+        namespace = "time",
+        description = "Converts source date to provided target timezone and return the date string",
+        parameters = {
+                @Parameter(name = "date.value",
+                        description = "The value of the date. " +
+                                "For example, `2014-11-11 13:23:44.657`, `2014-11-11`, `13:23:44.657`.",
+                        type = {DataType.STRING},
+                        dynamic = true,
+                        defaultValue = "-"),
+                @Parameter(name = "date.source.format",
+                        description = "The format input date.value." +
+                                "For example, `yyyy-MM-dd HH:mm:ss.SSS`. This is mandatory if you want to convert to " +
+                                "different timezone",
+                        type = {DataType.STRING},
+                        dynamic = true,
+                        defaultValue = "-"),
+                @Parameter(name = "target.timezone",
+                        description = "The timezone to which the target date need to be converted. " +
+                                "For example, `Asia/Kolkata`, `PST`. " +
+                                "Get the supported timezone IDs from [here]" +
+                                "(https://docs.oracle.com/javase/8/docs/api/java/time/ZoneId.html)",
+                        type = {DataType.STRING},
+                        dynamic = true,
+                        defaultValue = "-"),
+                @Parameter(name = "source.timezone",
+                        description = "The timezone to which the source time points to. " +
+                                "For example, `Asia/Kolkata`, `PST`. " +
+                                "Get the supported timezone IDs from [here]" +
+                                "(https://docs.oracle.com/javase/8/docs/api/java/time/ZoneId.html)",
+                        type = {DataType.STRING},
+                        dynamic = true,
+                        optional = true,
+                        defaultValue = "System timezone")
+
+        },
+        parameterOverloads = {
+                @ParameterOverload(parameterNames = {"date.value", "date.source.format", "target.timezone"}),
+                @ParameterOverload(parameterNames = {"date.value", "date.source.format", "target.timezone",
+                        "source.timezone"})
+        },
+        returnAttributes = @ReturnAttribute(
+                description = "Returns the converted date based on the target timezone in same source date format.",
+                type = {DataType.STRING}),
+        examples = {
+
+                @Example(
+                        syntax = "time:timezoneConvert('2014/11/11 13:23:44','yyyy/MM/dd HH:mm:ss','UTC', 'IST') ",
+                        description = "Converts date based on the target timezone `UTC` considering given source " +
+                                "timizone `IST` and returns `2014/11/11 07:53:44`."
+                ),
+
+                @Example(
+                        syntax = "time:timezoneConvert('2020-11-11 6:23:44', 'yyyy/MM/dd HH:mm:ss', 'CST') ",
+                        description = "Converts date based on the target timezone `CST` and since source timezone " +
+                                "is not given it will take system timezone as default and " +
+                                "returns `2020/11/10 18:53:44`."
+                )
+        }
+)
+public class TimezoneConvertFunctionExtension extends FunctionExecutor {
+
+    private Attribute.Type returnType = Attribute.Type.STRING;
+    private static final Logger log = Logger.getLogger(TimezoneConvertFunctionExtension.class);
+
+    @Override
+    protected StateFactory init(ExpressionExecutor[] attributeExpressionExecutors,
+                                ConfigReader configReader, SiddhiQueryContext siddhiQueryContext) {
+
+        if (attributeExpressionExecutors.length == 4) {
+            if (attributeExpressionExecutors[0].getReturnType() != Attribute.Type.STRING) {
+                throw new SiddhiAppValidationException("Invalid parameter type found for the first argument of " +
+                        "time:timezoneConvert(dateValue,dateSourceFormat,targetTimezone,sourceTimezone) function, " +
+                        "required " + Attribute.Type.STRING +
+                        " but found " + attributeExpressionExecutors[0].getReturnType().toString());
+            }
+            if (attributeExpressionExecutors[1].getReturnType() != Attribute.Type.STRING) {
+                throw new SiddhiAppValidationException("Invalid parameter type found for the second argument of " +
+                        "time:timezoneConvert(dateValue,dateSourceFormat,targetTimezone,sourceTimezone) function, " +
+                        "required " + Attribute.Type.STRING +
+                        " but found " + attributeExpressionExecutors[1].getReturnType().toString());
+            }
+            if (attributeExpressionExecutors[2].getReturnType() != Attribute.Type.STRING) {
+                throw new SiddhiAppValidationException("Invalid parameter type found for the third argument of " +
+                        "time:timezoneConvert(dateValue,dateSourceFormat,targetTimezone,sourceTimezone) function, " +
+                        "required " + Attribute.Type.STRING +
+                        " but found " + attributeExpressionExecutors[2].getReturnType().toString());
+            }
+            if (attributeExpressionExecutors[3].getReturnType() != Attribute.Type.STRING) {
+                throw new SiddhiAppValidationException("Invalid parameter type found for the fourth argument of " +
+                        "time:timezoneConvert(dateValue,dateSourceFormat,targetTimezone,sourceTimezone) function, " +
+                        "required " + Attribute.Type.STRING +
+                        " but found " + attributeExpressionExecutors[3].getReturnType().toString());
+            }
+        } else if (attributeExpressionExecutors.length == 3) {
+            if (attributeExpressionExecutors[0].getReturnType() != Attribute.Type.STRING) {
+                throw new SiddhiAppValidationException("Invalid parameter type found for the first argument of " +
+                        "time:timezoneConvert(dateValue,dateSourceFormat,targetTimezone,sourceTimezone) function, " +
+                        "required " + Attribute.Type.STRING +
+                        " but found " + attributeExpressionExecutors[0].getReturnType().toString());
+            }
+            if (attributeExpressionExecutors[1].getReturnType() != Attribute.Type.STRING) {
+                throw new SiddhiAppValidationException("Invalid parameter type found for the second argument of " +
+                        "time:timezoneConvert(dateValue,dateSourceFormat,targetTimezone,sourceTimezone) function, " +
+                        "required " + Attribute.Type.STRING +
+                        " but found " + attributeExpressionExecutors[1].getReturnType().toString());
+            }
+            if (attributeExpressionExecutors[2].getReturnType() != Attribute.Type.STRING) {
+                throw new SiddhiAppValidationException("Invalid parameter type found for the third argument of " +
+                        "time:timezoneConvert(dateValue,dateSourceFormat,targetTimezone,sourceTimezone) function, " +
+                        "required " + Attribute.Type.STRING +
+                        " but found " + attributeExpressionExecutors[2].getReturnType().toString());
+            }
+        } else {
+            throw new SiddhiAppValidationException("Invalid no of arguments passed to timezoneConvert() function, " +
+                    "required 3 or 4, but found " + attributeExpressionExecutors.length);
+        }
+        return null;
+    }
+
+    @Override
+    protected Object execute(Object[] data, State state) {
+
+        TimeZone srcTimeZone;
+        String sourceDate;
+        Date userSpecifiedSourceDate;
+
+        if (data.length == 3) {
+            if (data[0] == null || data[1] == null || data[2] == null) {
+                return null;
+            }
+            srcTimeZone = TimeZone.getDefault();
+        } else if (data.length == 4) {
+            if (data[0] == null || data[1] == null || data[2] == null || data[3] == null) {
+                return null;
+            }
+            srcTimeZone = TimeZone.getTimeZone((String) data[3]);
+        } else {
+            throw new SiddhiAppRuntimeException("Invalid set of arguments given to time:timezoneConvert() function." +
+                    "Arguments should be either 3 or 4. ");
+        }
+
+        sourceDate = (String) data[0];
+        SimpleDateFormat sourceFormatter = new SimpleDateFormat((String) data[1]);
+        SimpleDateFormat targetFormatter = new SimpleDateFormat((String) data[1]);
+        sourceFormatter.setTimeZone(srcTimeZone);
+        targetFormatter.setTimeZone(TimeZone.getTimeZone((String) data[2]));
+
+        try {
+            userSpecifiedSourceDate = sourceFormatter.parse(sourceDate);
+            return targetFormatter.format(userSpecifiedSourceDate);
+        } catch (ParseException e) {
+            String errorMsg = "Provided format " + data[1] + " does not match with the date value " +
+                    sourceDate + " " + e.getMessage();
+            throw new SiddhiAppRuntimeException(errorMsg, e);
+        }
+    }
+
+    @Override
+    protected Object execute(Object data, State state) {
+        return null;
+    }
+
+    @Override
+    public Attribute.Type getReturnType() {
+        return returnType;
+    }
+}
---
+++ component/src/test/java/io/siddhi/extension/execution/time/TimezoneConvertFunctionExtensionTestCase.java
@@ -0,0 +1,203 @@
+/*
+ *
+ *  * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *  *
+ *  * WSO2 Inc. licenses this file to you under the Apache License,
+ *  * Version 2.0 (the "License"); you may not use this file except
+ *  * in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied. See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package io.siddhi.extension.execution.time;
+
+import io.siddhi.core.SiddhiAppRuntime;
+import io.siddhi.core.SiddhiManager;
+import io.siddhi.core.event.Event;
+import io.siddhi.core.query.output.callback.QueryCallback;
+import io.siddhi.core.stream.input.InputHandler;
+import io.siddhi.core.util.EventPrinter;
+import org.apache.log4j.Logger;
+import org.testng.AssertJUnit;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TimezoneConvertFunctionExtensionTestCase {
+
+    private static Logger log = Logger.getLogger(TimezoneConvertFunctionExtensionTestCase.class);
+    private volatile boolean eventArrived;
+    private AtomicInteger eventCount;
+
+    @BeforeMethod
+    public void init() {
+        eventArrived = false;
+        eventCount = new AtomicInteger(0);
+    }
+
+    @Test
+    public void timezoneConvertFunctionWithAllArgumentsTestCase() throws InterruptedException {
+
+        log.info("timezoneConvertFunctionWithAllArgumentsTestCase");
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        String inStreamDefinition = "define stream inputStream (symbol string,"
+                + "dateValue string,sourceFormat string,targetTimezone string,sourceTimezone string);";
+        String query = ("@info(name = 'query1') from inputStream select symbol , "
+                + "time:timezoneConvert(dateValue,sourceFormat,targetTimezone,sourceTimezone) as convertedDate "
+                + "insert into outputStream;");
+        SiddhiAppRuntime siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(inStreamDefinition + query);
+
+        siddhiAppRuntime.addCallback("query1", new QueryCallback() {
+            @Override
+            public void receive(long timeStamp, Event[] inEvents, Event[] removeEvents) {
+                EventPrinter.print(timeStamp, inEvents, removeEvents);
+                eventArrived = true;
+                for (Event inEvent : inEvents) {
+                    eventCount.incrementAndGet();
+                    if (eventCount.intValue() == 1) {
+                        AssertJUnit.assertEquals("2014-11-10 23:53:44", inEvent.getData(1));
+                    }
+                    if (eventCount.intValue() == 2) {
+                        AssertJUnit.assertEquals("2016-04-20T14:30:00-0400", inEvent.getData(1));
+                    }
+                }
+            }
+        });
+
+        InputHandler inputHandler = siddhiAppRuntime.getInputHandler("inputStream");
+        siddhiAppRuntime.start();
+        inputHandler.send(new Object[] {
+                "IBM", "2014-11-11 13:23:44", "yyyy-MM-dd HH:mm:ss", "PST", "IST"});
+        inputHandler.send(new Object[] {
+                "IBM", "2016-04-21T00:00:00+0530", "yyyy-MM-dd'T'HH:mm:ssZ", "America/New_York", "IST"});
+        Thread.sleep(100);
+        AssertJUnit.assertEquals(2, eventCount.get());
+        AssertJUnit.assertTrue(eventArrived);
+        siddhiAppRuntime.shutdown();
+    }
+
+    @Test
+    public void timezoneConvertFunctionWithDifferentTimezonesArgumentsTestCase() throws InterruptedException {
+
+        log.info("timezoneConvertFunctionWithDifferentTimezonesArgumentsTestCase");
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        String inStreamDefinition = "define stream inputStream (symbol string,"
+                + "dateValue string,sourceFormat string,targetTimezone string,sourceTimezone string);";
+        String query = ("@info(name = 'query1') from inputStream select symbol , "
+                + "time:timezoneConvert(dateValue,sourceFormat,targetTimezone,sourceTimezone) as convertedDate "
+                + "insert into outputStream;");
+        SiddhiAppRuntime siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(inStreamDefinition + query);
+
+        siddhiAppRuntime.addCallback("query1", new QueryCallback() {
+            @Override
+            public void receive(long timeStamp, Event[] inEvents, Event[] removeEvents) {
+                EventPrinter.print(timeStamp, inEvents, removeEvents);
+                eventArrived = true;
+                for (Event inEvent : inEvents) {
+                    eventCount.incrementAndGet();
+                    if (eventCount.intValue() == 1) {
+                        AssertJUnit.assertEquals("2020-11-17 16:51:19", inEvent.getData(1));
+                    }
+                    if (eventCount.intValue() == 2) {
+                        AssertJUnit.assertEquals("2016-04-20T14:30:00-0400", inEvent.getData(1));
+                    }
+                    if (eventCount.intValue() == 3) {
+                        AssertJUnit.assertEquals("2020-11-18 04:54:19", inEvent.getData(1));
+                    }
+                    if (eventCount.intValue() == 4) {
+                        AssertJUnit.assertEquals("2020-11-18 14:26", inEvent.getData(1));
+                    }
+                    if (eventCount.intValue() == 5) {
+                        AssertJUnit.assertEquals("2020-11-18 01:26", inEvent.getData(1));
+                    }
+                    if (eventCount.intValue() == 6) {
+                        AssertJUnit.assertEquals("2014/11/11 07:53:44", inEvent.getData(1));
+                    }
+                }
+            }
+        });
+
+        InputHandler inputHandler = siddhiAppRuntime.getInputHandler("inputStream");
+        siddhiAppRuntime.start();
+        inputHandler.send(new Object[] {
+                "IBM", "2020-11-18 6:21:19", "yyyy-MM-dd HH:mm:ss", "PST", "IST"});
+        inputHandler.send(new Object[] {
+                "IBM", "2016-04-21T00:00:00+0530", "yyyy-MM-dd'T'HH:mm:ssZ", "America/New_York", "IST"});
+        inputHandler.send(new Object[] {
+                "IBM", "2020-11-18 6:54:19", "yyyy-MM-dd HH:mm:ss", "PST", "CST"});
+        inputHandler.send(new Object[] {
+                "IBM", "2020-11-18 6:56:20", "yyyy-MM-dd HH:mm", "NST", "IST"});
+        inputHandler.send(new Object[] {
+                "IBM", "2020-11-18 6:56:20", "yyyy-MM-dd HH:mm", "UTC", "IST"});
+        inputHandler.send(new Object[] {
+                "IBM", "2014/11/11 13:23:44", "yyyy/MM/dd HH:mm:ss", "UTC", "IST"});
+        inputHandler.send(new Object[] {
+                "IBM", "2019-08-07 07:19:10.02", "yyyy-MM-dd HH:mm:ss", "UTC", "PT"});
+        Thread.sleep(100);
+        AssertJUnit.assertEquals(6, eventCount.get());
+        AssertJUnit.assertTrue(eventArrived);
+        siddhiAppRuntime.shutdown();
+    }
+
+    @Test
+    public void timezoneConvertFunctionWithMandatoryArgumentsTestCase() throws InterruptedException {
+
+        log.info("timezoneConvertFunctionWithMandatoryArgumentsTestCase");
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        String inStreamDefinition = "define stream inputStream (symbol string,"
+                + "dateValue string,sourceFormat string,targetTimezone string,sourceTimezone string);";
+        String query = ("@info(name = 'query1') from inputStream select symbol , "
+                + "time:timezoneConvert(dateValue,sourceFormat,targetTimezone) as convertedDate "
+                + "insert into outputStream;");
+        SiddhiAppRuntime siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(inStreamDefinition + query);
+
+        siddhiAppRuntime.addCallback("query1", new QueryCallback() {
+            @Override
+            public void receive(long timeStamp, Event[] inEvents, Event[] removeEvents) {
+                EventPrinter.print(timeStamp, inEvents, removeEvents);
+                eventArrived = true;
+                for (Event inEvent : inEvents) {
+                    eventCount.incrementAndGet();
+                    if (eventCount.intValue() == 1) {
+                        AssertJUnit.assertEquals("2014-11-10 23:53:44", inEvent.getData(1));
+                    }
+                    if (eventCount.intValue() == 2) {
+                        AssertJUnit.assertEquals("2016-04-20T14:30:00-0400", inEvent.getData(1));
+                    }
+                    if (eventCount.intValue() == 3) {
+                        AssertJUnit.assertEquals("2020/11/10 18:53:44", inEvent.getData(1));
+                    }
+
+                }
+            }
+        });
+
+        InputHandler inputHandler = siddhiAppRuntime.getInputHandler("inputStream");
+        siddhiAppRuntime.start();
+        inputHandler.send(new Object[] {
+                "IBM", "2014-11-11 13:23:44", "yyyy-MM-dd HH:mm:ss", "PST"});
+        inputHandler.send(new Object[] {
+                "IBM", "2016-04-21T00:00:00+0530", "yyyy-MM-dd'T'HH:mm:ssZ", "America/New_York"});
+        inputHandler.send(new Object[] {
+                "IBM", "2020/11/11 6:23:44", "yyyy/MM/dd HH:mm:ss", "CST"});
+        Thread.sleep(100);
+        AssertJUnit.assertEquals(3, eventCount.get());
+        AssertJUnit.assertTrue(eventArrived);
+        siddhiAppRuntime.shutdown();
+    }
+
+
+}
